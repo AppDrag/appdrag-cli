f03d0fcf89b9d52a7cec691e864ffc68a6f710b9
wass08 <wass08@gmail.com>
2022-08-19 17:40:55 +0200

    fix api push on a specific function

--- src/commands/api/api.js
+++ src/commands/api/api.js
@@ -1,7 +1,19 @@
-const { setupCheck, config, currFolder, refreshToken, tokenObj } = require('../../utils/common');
-const { getFunctionsList, parseFunctions, writeScriptFile, apiJson, pushFunctions } = require('../../utils/api/api');
-const fs = require('fs');
-const chalk = require('chalk');
+const {
+  setupCheck,
+  config,
+  currFolder,
+  refreshToken,
+  tokenObj,
+} = require("../../utils/common");
+const {
+  getFunctionsList,
+  parseFunctions,
+  writeScriptFile,
+  apiJson,
+  pushFunctions,
+} = require("../../utils/api/api");
+const fs = require("fs");
+const chalk = require("chalk");
 
 const pushApi = async (args, argOpts) => {
   let appId = setupCheck(argOpts);
@@ -9,11 +21,11 @@ const pushApi = async (args, argOpts) => {
     return;
   }
   let token = tokenObj.token;
-  let basePath = 'CloudBackend/code/';
+  let basePath = "CloudBackend/code/";
   let folders = fs.readdirSync(basePath);
   if (args[0]) {
-    if (folders.includes(args[0])) {
-      folders = [...args[0]];
+    if (folders.includes(args[0].toString())) {
+      folders = [args[0].toString()];
     }
   }
   await pushFunctions(appId, token, currFolder, basePath, folders);
@@ -26,17 +38,21 @@ const pullApi = async (args, argOpts) => {
   }
   let token = tokenObj.token;
   let response = await getFunctionsList(appId, token);
-  if (response.status == 'KO') {
-    if (tokenObj.method == 'login') {
-      let token_ref = config.get('refreshToken');
+  if (response.status == "KO") {
+    if (tokenObj.method == "login") {
+      let token_ref = config.get("refreshToken");
       await refreshToken(token_ref);
       response = await getFunctionsList(appId, token);
-      if (response.status == 'KO') {
-        console.log(chalk.red('Please log-in again'));
+      if (response.status == "KO") {
+        console.log(chalk.red("Please log-in again"));
         return;
       }
     } else {
-      console.log(chalk.red('The token used through the -t option may be incorrect/invalid.'));
+      console.log(
+        chalk.red(
+          "The token used through the -t option may be incorrect/invalid."
+        )
+      );
       return;
     }
   }
@@ -44,12 +60,12 @@ const pullApi = async (args, argOpts) => {
   // If function id specified
   if (args[0]) {
     let func = functionList.find((func) => func.id == args[0]);
-    functionList = [{...func}];
-  } 
+    functionList = [{ ...func }];
+  }
   parseFunctions(token, appId, functionList);
   writeScriptFile(functionList);
-  fs.writeFileSync('api.json', apiJson(functionList, appId));
-  console.log(chalk.green('Done writing JSON files.'));
+  fs.writeFileSync("api.json", apiJson(functionList, appId));
+  console.log(chalk.green("Done writing JSON files."));
   return true;
 };
 
